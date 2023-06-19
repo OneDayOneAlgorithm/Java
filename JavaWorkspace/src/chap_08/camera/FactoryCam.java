725b0d6cd589bbefb75520e883d2167016fef919
Kim hyeongjin <127001416+OneDayOneAlgorithm@users.noreply.github.com>
2023-06-19 23:00:02 +0900

    인터페이스

---
+++ JavaWorkspace/src/chap_08/camera/FactoryCam.java
@@ -0,0 +1,23 @@
+package chap_08.camera;
+
+import chap_08.detector.Detectable;
+import chap_08.reporter.Reportable;
+
+public class FactoryCam extends Camera implements Detectable, Reportable {
+    public Detectable detectable;
+    public Reportable reporter;
+    @Override
+    public void showMainFeature() {
+        System.out.println("화재 감지");
+    }
+
+    @Override
+    public void detect() {
+
+    }
+
+    @Override
+    public void report() {
+
+    }
+}
