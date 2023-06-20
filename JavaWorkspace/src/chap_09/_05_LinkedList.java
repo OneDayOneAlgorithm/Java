f903ed6ec03a849cb775df6c4359854b371972d1
Kim hyeongjin <127001416+OneDayOneAlgorithm@users.noreply.github.com>
2023-06-20 23:00:44 +0900

    iterator

--- JavaWorkspace/src/chap_09/_05_LinkedList.java
+++ JavaWorkspace/src/chap_09/_05_LinkedList.java
@@ -1,5 +1,6 @@
 package chap_09;
 
+import java.util.Collections;
 import java.util.LinkedList;
 
 public class _05_LinkedList {
@@ -36,9 +37,65 @@ public class _05_LinkedList {
 
         System.out.println("----------------------");
 
-        System.out.println("학생 추가 전 : " + list.get(1));
+        System.out.println("학생 추가 전 (index 1) : " + list.get(1));
         list.add(1,"김영철");
-        System.out.println("학생 추가 후 : " + list.get(1));
-        System.out.println("학생 추가 후 : " + list.get(2));
+        System.out.println("학생 추가 후 (index 1) : " + list.get(1));
+        System.out.println("학생 추가 후 (index 1) : " + list.get(2));
+
+        // 삭제
+        System.out.println("남은 학생 수 (제외 전) : " + list.size());
+        list.remove(list.size()-1);
+        System.out.println("남은 학생 수 (제외 후) : " + list.size());
+
+        System.out.println("----------------------");
+
+        for (String s : list){
+            System.out.println(s);
+        }
+        // 처음 학생과 마지막 학생이 전학
+        System.out.println("남은 학생 수 (전학 전) : " + list.size());
+        list.removeFirst(); // 처음 데이터
+        list.removeLast(); // 마지막 데이터
+        System.out.println("남은 학생 수 (전학 후) : " + list.size());
+        for (String s : list){
+            System.out.println(s);
+        }
+
+        System.out.println("----------------------");
+
+        // 변경 (수강권 양도)
+        list.set(0, "이수근");
+        System.out.println(list.get(0));
+        
+        // 확인
+        System.out.println(list.indexOf("김종국"));
+        if (list.contains("김종국")){
+            System.out.println("수강 신청 성공");
+        }else{
+            System.out.println("수강 신청 실패");
+        }
+
+        System.out.println("----------------------");
+
+        // 전체 삭제
+        list.clear();
+        if (list.isEmpty()){
+            System.out.println("학생 수 : " + list.size());
+            System.out.println("리스트가 비었습니다.");
+        }
+
+        System.out.println("----------------------");
+
+        // 새로운 학기
+        list.add("유재석");
+        list.add("조세호");
+        list.add("김종국");
+        list.add("박명수");
+        list.add("강호동");
+
+        Collections.sort(list); // 정렬
+        for (String s : list) {
+            System.out.println(s);
+        }
     }
 }
